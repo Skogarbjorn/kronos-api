ac7826e08274e82e16729f6ac7094aec6ed46ed8
Skogarbjorn <ragnarbjorn88@gmail.com>
2026-02-04 12:27:41 -0100

    readme + standardization (might bite me in the back later

--- README.md
+++ README.md
@@ -1,8 +1,13 @@
 # todo!
 
 - [ ] Clock in / Clock out
+  - [x] Main functionality
+  - [x] No duplicate / cold clockout
+  - [ ] Too long shifts
+  - [ ] No negative duration
 - [x] Authenticated routes middleware
 - [ ] Shift overview
 - [ ] Shift history (will expand into parts later)
 - [ ] Get locations/companies/tasks
 - [ ] Get contracted company+workspaces
+- [x] Error logging + error responses
--- internal/auth/routes.go
+++ internal/auth/routes.go
@@ -2,79 +2,94 @@ package auth
 
 import (
 	"database/sql"
-	"encoding/json"
 	"net/http"
+	"test/internal/abstractions"
 )
 
+//func RegisterHandler(db *sql.DB) http.HandlerFunc {
+//	return func(w http.ResponseWriter, r *http.Request) {
+//		var input UserCreate
+//		if err := json.NewDecoder(r.Body).Decode(&input); err != nil {
+//			http.Error(w, "invalid body", http.StatusBadRequest)
+//			return
+//		}
+//
+//		user, err := CreateUser(r.Context(), db, input)
+//		if err != nil {
+//			http.Error(w, err.Error(), http.StatusInternalServerError)
+//			return
+//		}
+//
+//		json.NewEncoder(w).Encode(user)
+//	}
+//}
+//
+//func LoginHandler(db *sql.DB) http.HandlerFunc {
+//	return func(w http.ResponseWriter, r *http.Request) {
+//		var input UserPinAuth
+//		if err := json.NewDecoder(r.Body).Decode(&input); err != nil {
+//			http.Error(w, "invalid body", http.StatusBadRequest)
+//			return
+//		}
+//
+//		res, err := ColdStartPin(r.Context(), db, input)
+//		if err != nil {
+//			http.Error(w, err.Error(), http.StatusInternalServerError)
+//			return
+//		}
+//
+//		json.NewEncoder(w).Encode(res)
+//	}
+//}
+//
+//func SilentRefreshHandler(db *sql.DB) http.HandlerFunc {
+//	return func(w http.ResponseWriter, r *http.Request) {
+//		var input UserSilentRefresh
+//		if err := json.NewDecoder(r.Body).Decode(&input); err != nil {
+//			http.Error(w, "invalid body", http.StatusBadRequest)
+//			return
+//		}
+//
+//		tokens, err := RefreshTokens(r.Context(), db, input)
+//		if err != nil {
+//			http.Error(w, err.Error(), http.StatusInternalServerError)
+//			return
+//		}
+//
+//		json.NewEncoder(w).Encode(tokens)
+//	}
+//}
+
+//func ReAuthHandler(db *sql.DB) http.HandlerFunc {
+//	return func(w http.ResponseWriter, r *http.Request) {
+//		var input UserReAuth
+//		if err := json.NewDecoder(r.Body).Decode(&input); err != nil {
+//			http.Error(w, "invalid body", http.StatusBadRequest)
+//			return
+//		}
+//
+//		tokens, err := WarmStartPin(r.Context(), db, input)
+//		if err != nil {
+//			http.Error(w, err.Error(), http.StatusInternalServerError)
+//			return
+//		}
+//
+//		json.NewEncoder(w).Encode(tokens)
+//	}
+//}
+
 func RegisterHandler(db *sql.DB) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		var input UserCreate
-		if err := json.NewDecoder(r.Body).Decode(&input); err != nil {
-			http.Error(w, "invalid body", http.StatusBadRequest)
-			return
-		}
-
-		user, err := CreateUser(r.Context(), db, input)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		json.NewEncoder(w).Encode(user)
-	}
+	return abstractions.CreateJSONHandler(db, CreateUser, WriteDomainError)
 }
 
 func LoginHandler(db *sql.DB) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		var input UserPinAuth
-		if err := json.NewDecoder(r.Body).Decode(&input); err != nil {
-			http.Error(w, "invalid body", http.StatusBadRequest)
-			return
-		}
-
-		res, err := ColdStartPin(r.Context(), db, input)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		json.NewEncoder(w).Encode(res)
-	}
+	return abstractions.CreateJSONHandler(db, ColdStartPin, WriteDomainError)
 }
 
 func SilentRefreshHandler(db *sql.DB) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		var input UserSilentRefresh
-		if err := json.NewDecoder(r.Body).Decode(&input); err != nil {
-			http.Error(w, "invalid body", http.StatusBadRequest)
-			return
-		}
-
-		tokens, err := RefreshTokens(r.Context(), db, input)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		json.NewEncoder(w).Encode(tokens)
-	}
+	return abstractions.CreateJSONHandler(db, RefreshTokens, WriteDomainError)
 }
 
 func ReAuthHandler(db *sql.DB) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		var input UserReAuth
-		if err := json.NewDecoder(r.Body).Decode(&input); err != nil {
-			http.Error(w, "invalid body", http.StatusBadRequest)
-			return
-		}
-
-		tokens, err := WarmStartPin(r.Context(), db, input)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		json.NewEncoder(w).Encode(tokens)
-	}
+	return abstractions.CreateJSONHandler(db, WarmStartPin, WriteDomainError)
 }
-
